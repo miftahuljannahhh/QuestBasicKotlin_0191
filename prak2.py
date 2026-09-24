284dfccf81e36ef6a22177ff60045a8ad18cb615
miftahuljannahhh <mj5416386@gmail.com>
2026-09-24 18:43:42 +0700

    first commit

---
+++ prak2.py
@@ -0,0 +1,9 @@
+when (obj) {
+ // Checks whether obj equals to "1"
+ "1" -> println("One")
+ // Checks whether obj equals to "Hello"
+ "Hello" -> println("Greeting")
+ // Default statement
+ else -> println("Unknown")
+}
+// Greeting
